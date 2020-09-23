9ae80e07b252c0505c4a080f7f3ee1ac93c1d68f
om prakash <omprakash@loved.com>
2020-09-23 15:38:04 +0530

    stable vector icon for loved app

--- directory/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn directory header', () => {
-  const { getByText } = render(<App />);
-  const headerElement = getByText(/react-native-vector-icons directory/i);
-  expect(headerElement).toBeInTheDocument();
-});
